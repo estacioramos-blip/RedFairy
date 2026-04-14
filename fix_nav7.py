aabf1050b34da2595b7b29e8a83cd1147d43ceda
Estacio Ramos <estacio.ramos@gmail.com>
2026-04-14 11:05:39 -0300

    fix nav: style inline com width 100vw

---
+++ fix_nav7.py
@@ -0,0 +1,17 @@
+filepath = 'C:/Users/Estacio/Desktop/redfairy/src/components/LandingPage.jsx'
+
+with open(filepath, encoding='utf-8') as f:
+    txt = f.read()
+
+old = '<nav id="landing-nav" className={navScrolled ? \'scrolled\' : \'\'}>'
+new = '<nav id="landing-nav" className={navScrolled ? \'scrolled\' : \'\'} style={{ position:\'fixed\', top:0, left:0, right:0, width:\'100vw\', maxWidth:\'100vw\', boxSizing:\'border-box\', zIndex:1000, display:\'flex\', justifyContent:\'space-between\', alignItems:\'center\', padding:\'0.75rem 2rem\' }}>'
+
+if old in txt:
+    txt = txt.replace(old, new)
+    open(filepath, 'w', encoding='utf-8').write(txt)
+    print('OK: style inline adicionado na nav')
+else:
+    print('ERRO: tag nav nao encontrada')
+    idx = txt.find('id="landing-nav"')
+    if idx >= 0:
+        print('Trecho:', repr(txt[idx:idx+100]))
