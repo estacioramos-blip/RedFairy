ba2707d15ffe42192f41f02789c34fa36fce2f7f
Estacio Ramos <estacio.ramos@gmail.com>
2026-04-19 04:24:48 -0300

    limpeza: remover scripts temporarios

--- inspect_cm.py
+++
@@ -1,5 +0,0 @@
-calc = 'C:/Users/Estacio/Desktop/redfairy/src/components/Calculator.jsx'
-with open(calc, encoding='utf-8') as f:
-    txt = f.read()
-idx = txt.find('carregarMedico')
-print(repr(txt[idx:idx+200]))
--- inspect_oba_flow2.py
+++
@@ -1,9 +0,0 @@
-lp = 'C:/Users/Estacio/Desktop/redfairy/src/components/LandingPage.jsx'
-with open(lp, encoding='utf-8') as f:
-    txt = f.read()
-
-idx = txt.find('Sou Bariátrico')
-# Pegar o botão completo — procurar o <button que contém esse texto
-start = txt.rfind('<button', 0, idx)
-end = txt.find('</button>', idx) + 9
-print(repr(txt[start:end]))
