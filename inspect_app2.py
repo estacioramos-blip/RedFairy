91ce4852f2a6745ce44239487df4f8f29527b708
Estacio Ramos <estacio.ramos@gmail.com>
2026-04-19 16:21:49 -0300

    limpeza: remove scripts .py da raiz

--- inspect_app2.py
+++
@@ -1,4 +0,0 @@
-path = 'C:/Users/Estacio/Desktop/redfairy/src/App.jsx'
-with open(path, encoding='utf-8') as f:
-    txt = f.read()
-print(txt)
--- inspect_carregar.py
+++
@@ -1,5 +0,0 @@
-dash = 'C:/Users/Estacio/Desktop/redfairy/src/components/PatientDashboard.jsx' 
-with open(dash, encoding='utf-8') as f: 
-    txt = f.read() 
-idx = txt.find('async function carregarDados') 
-print(repr(txt[idx:idx+600])) 
